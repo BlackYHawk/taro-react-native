03761c75f42cc8bc371823a88b0ac14e76d35943
nickyefei <shoxvc2008@163.com>
2021-01-29 18:19:23 +0800

    feat: 权限功能联调

--- packages/taro-rn/src/lib/animate.ts
+++ packages/taro-rn/src/lib/animate.ts
@@ -1,12 +1,12 @@
 import { Animated, Easing } from "react-native";
 
 export const animate = (selector, keyframes, duration, callback) => {
-  console.log(selector)
+  console.log('hello animate')
   // console.log(selector['_internalFiberInstanceHandleDEV'].memoizedProps.style)
   // console.log(keyframes)
-  keyframes.forEach(item => {
-    selector['_internalFiberInstanceHandleDEV'].memoizedProps.style = item
-  })
-  console.log(selector)
+  // keyframes.forEach(item => {
+  //   selector['_internalFiberInstanceHandleDEV'].memoizedProps.style = item
+  // })
+  // console.log(selector)
   // return {}
 }
--- packages/taro-rn/src/lib/permission.ts
+++ packages/taro-rn/src/lib/permission.ts
@@ -1,26 +1,33 @@
-import { Alert ,Linking, Platform, AppState } from 'react-native';
+import { Linking, AppState } from 'react-native';
 import * as Permissions from 'expo-permissions';
-// import * as Location from 'expo-location';
 
 const scopeMap = {
   'scope.userLocation': Permissions.LOCATION,
   'scope.record': Permissions.AUDIO_RECORDING,
-  'scope.writePhotosAlbum': (Permissions as any).MEDIA_LIBRARY,
+  'scope.writePhotosAlbum': Permissions.CAMERA_ROLL,
   'scope.camera': Permissions.CAMERA,
-  'scope.NOTIFICATIONS': Permissions.NOTIFICATIONS,
-  'scope.USER_FACING_NOTIFICATIONS': Permissions.USER_FACING_NOTIFICATIONS,
-  'scope.CONTACTS': Permissions.CONTACTS,
-  'scope.CALENDAR': Permissions.CALENDAR,
+  // 'scope.NOTIFICATIONS': Permissions.NOTIFICATIONS,
+  // 'scope.USER_FACING_NOTIFICATIONS': Permissions.USER_FACING_NOTIFICATIONS,
+  // 'scope.CONTACTS': Permissions.CONTACTS,
+  // 'scope.CALENDAR': Permissions.CALENDAR,
   // 'scope.REMINDERS': Permissions.REMINDERS, // ios only
-  'scope.SYSTEM_BRIGHTNESS': Permissions.SYSTEM_BRIGHTNESS
+  // 'scope.SYSTEM_BRIGHTNESS': Permissions.SYSTEM_BRIGHTNESS
 }
 
+let stateListener // 缓存监听函数
+
 const getAuthSetting = async () => {
-  const scopeArr = Object.keys(scopeMap).map(key => scopeMap[key])
+  const keyArr = Object.keys(scopeMap)
+  const scopeArr = keyArr.map(key => scopeMap[key])
   let auths = {}
   const { permissions } = await Permissions.getAsync(...scopeArr)
-  Object.keys(permissions).forEach(key => {
-    auths[key] = permissions[key].status === 'granted'
+  console.log(permissions)
+  Object.keys(permissions).forEach(pkey => {
+    keyArr.forEach((skey) => {
+      if (scopeMap[skey] === pkey) {
+        auths[skey] = permissions[pkey].status === 'granted'
+      }
+    })
   })
   return auths
 }
@@ -29,18 +36,15 @@ const handleAppStateChange = async (nextAppState, resolve, reject, opts) => {
   const { success, fail, complete } = opts
   const res: any = {}
 
-  if (
-    AppState.currentState.match(/inactive|background/) &&
-    nextAppState === "active"
-  ) {
-    console.log("App has come to the foreground!");
+  if (AppState.currentState === 'active') {
+    console.log('App has come to the foreground!');
     try {
       res.authSetting = await getAuthSetting()
       res.errMsg = 'openSetting:ok'
       success && success(res)
       complete && complete(res)
   
-      AppState.removeEventListener("change", handleAppStateChange as any);
+      AppState.removeEventListener('change', stateListener as any);
       resolve(res)
     } catch (error) {
       res.errMsg = 'openSetting:fail'
@@ -50,16 +54,12 @@ const handleAppStateChange = async (nextAppState, resolve, reject, opts) => {
       reject(error)
     }
   }
-  AppState.currentState = nextAppState;
+  // AppState.currentState = nextAppState;
 };
 
 export function authorize(opts: Taro.authorize.Option): Promise<Taro.General.CallbackResult> {
   const { scope, success, fail, complete } = opts
   const res: any = {}
-
-  if (Platform.OS === 'ios') {
-
-  }
 
   return new Promise(async (resolve, reject) => {
     try {
@@ -71,7 +71,7 @@ export function authorize(opts: Taro.authorize.Option): Promise<Taro.General.Cal
 
         resolve(res)
       } else {
-        res.errMsg = 'authorize:not granted'
+        res.errMsg = 'authorize:denied/undetermined'
         fail && fail(res)
         complete && complete(res)
 
@@ -90,10 +90,6 @@ export function authorize(opts: Taro.authorize.Option): Promise<Taro.General.Cal
 export function getSetting(opts: Taro.getSetting.Option): Promise<Taro.getSetting.SuccessCallbackResult> {
   const { success, fail, complete } = opts
   const res: any = {}
-
-  if (Platform.OS === 'ios') {
-
-  }
 
   return new Promise(async (resolve, reject) => {
     try {
@@ -114,12 +110,9 @@ export function getSetting(opts: Taro.getSetting.Option): Promise<Taro.getSettin
 }
 
 export function openSetting(opts: Taro.openSetting.Option): Promise<Taro.openSetting.SuccessCallbackResult> {
-  if (Platform.OS === 'ios') {
-
-  }
-
   return new Promise(async (resolve, reject) => {
-    AppState.addEventListener("change", (next) => handleAppStateChange(next, resolve, reject, opts))
+    stateListener = (next) => handleAppStateChange(next, resolve, reject, opts)
+    AppState.addEventListener('change', stateListener)
     Linking.openSettings()
   })
 }
